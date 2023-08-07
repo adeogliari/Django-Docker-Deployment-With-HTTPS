876b7982adc38ad2deda4c911dd905bd2ba6c073
Ademir Ogliari <ademir.ogliari@saude.gov.br>
2023-08-07 08:51:12 -0300

    Arquivos limpos

--- docker/django/app/app/urls.py
+++ docker/django/app/app/urls.py
@@ -18,11 +18,8 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
-from home import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
 ]
 
 if settings.DEBUG:
--- docker/django/app/core/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from core.models import Sample
-
-# Register your models here.
-admin.site.register(Sample)
